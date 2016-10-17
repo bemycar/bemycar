1163d4639119e43b7f336875c57ce541576a1285
bemycar <yipstudios@gmail.com>
2016-10-17 08:50:30 +0100

    home screen

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ include 'header.php';
 ?>
 <div class="c-home">
   <div class="o-layout-container">
+  	<img style = "margin-top:10%;margin-left:5%;" height = "30%" width = "30%"  src = "img/arrow.png"></img>
     <div class="o-search-field">
 
       <form class="c-home__search" id = "search_form" action ="search.php" method="POST">
-        <input class="search-box" id="carword" name="carword" placeholder="Search carword" type="text" autocomplete="off">
+        <input class="search-box" id="carword" name="carword" placeholder="Search Carword.." type="text" autocomplete="off">
         <button class="c-btn c-btn--search" id='signup_button' type='button' id='login'>GO</button>
       </form>
 
